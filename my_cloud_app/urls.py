76b37002da02e290754cb3f836f17453219267c6
Your Name <you@example.com>
2023-12-09 13:28:34 +0300

    com 09.12

--- my_cloud_app/urls.py
+++ my_cloud_app/urls.py
@@ -2,18 +2,23 @@ from django.urls import path
 # from rest_framework import routers
 
 from my_cloud_app.views import login_user, logout_user, register_user, list_users, delete_user, get_list_files, \
-    delete_file, get_list_files_admin, upload_file
+    delete_file, get_list_files_admin, upload_file, rename_file, edit_description_file, download_file, \
+    download_file_from_link
 
 # router = routers.DefaultRouter()
 urlpatterns = [
     path('login/', login_user),
     path('logout/', logout_user),
     path('register/', register_user),
-    path('admin/get_users/', list_users),
-    path('admin/delete_user/', delete_user),
+    path('upload_file/', upload_file),
+    path('rename_file/', rename_file),
     path('get_files/', get_list_files),
     path('delete_file/', delete_file),
+    path('edit_description_file/', edit_description_file),
+    path('admin/get_users/', list_users),
+    path('admin/delete_user/', delete_user),
     path('admin/get_user_files/', get_list_files_admin),
-    path('upload_file/', upload_file)
+    path('download_file/', download_file),
+    path('public/download_file/', download_file_from_link)
 ]
 
--- my_cloud_app/views.py
+++ my_cloud_app/views.py
@@ -1,34 +1,25 @@
+import random
+import string
+
 from django.contrib.auth import authenticate
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.files.storage import FileSystemStorage
-from django.shortcuts import render
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes, parser_classes
 from rest_framework.parsers import MultiPartParser, FileUploadParser
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework import status
 
 from .models import User, File
 from .permissions import IsAdmin
 from .serializers import UserSerializer, FullUserSerializer, FileSerializer
-
-
+from .config import BASE_DIR_STORAGE
+from .utils import *
 
 
 # Create your views here.
 
 # блок api для работы с пользователями
-def create_response(status_code, message, success=False, data=None):
-    response = {
-        "status": status_code,
-        "message": message,
-        "success": success
-    }
-
-    if data:
-        response["data"] = data
-    return Response(status=status_code, data=response)
 
 
 @api_view(['POST'])
@@ -37,6 +28,9 @@ def register_user(request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            user = User.objects.get(login=serializer.initial_data['login'])
+            user.path = f'/{user.id}'
+            user.save(update_fields=['path'])
             return create_response(status.HTTP_201_CREATED,
                                    'Пользователь успешно зарегистрирован',
                                    True,
@@ -83,10 +77,8 @@ def logout_user(request):
 def list_users(request):
     if request.method == 'GET':
         try:
-            users = User.objects.values_list('login', 'username', 'email', 'is_admin')
-            # print(users)
+            users = User.objects.all().values('login', 'username', 'email', 'is_admin')
             serializer_users = FullUserSerializer(users, many=True)
-            serializer_users.is_valid()
             result = serializer_users.data
             return create_response(status.HTTP_200_OK,
                                    'Получен список пользователей',
@@ -129,21 +121,21 @@ def delete_user(request):
 
 # блок api для работы с файловым хранилищем
 
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_list_files(request):
     if request.method == 'GET':
         try:
-            serializer = FullUserSerializer(data=request.data)
-            user = User.objects.get(pk=serializer.initial_data['id'])
-            files = File.objects.filter(user=user).values_list()
+            user_id = request.GET.get("id")
+            user = User.objects.get(pk=user_id)
+            files = File.objects.filter(user=user).values()
             return create_response(status.HTTP_200_OK,
                                    'Получен список файлов пользователя',
                                    True,
                                    {'files': files})
         except Exception as e:
-            return create_response(status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                   str(e))
+            return create_response(status.HTTP_500_INTERNAL_SERVER_ERROR, str(e))
 
 
 @api_view(['GET'])
@@ -153,24 +145,46 @@ def get_list_files_admin(request):
         try:
             user_id = request.GET.get("user_id")
             user = User.objects.get(pk=user_id)
-            files = File.objects.filter(user=user).values_list()
+            files = File.objects.filter(user=user).values()
             return create_response(status.HTTP_200_OK,
                                    'Получен список файлов пользователя',
                                    True,
                                    {'files': files})
         except Exception as e:
-            return create_response(status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                   str(e))
+            return create_response(status.HTTP_500_INTERNAL_SERVER_ERROR, str(e))
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 @parser_classes([MultiPartParser, FileUploadParser])
 def upload_file(request):
     if request.method == 'POST':
-        file = request.FILES['file']
-        print(FileSystemStorage(location='/my_cloud_app/storage').save(file.name, file))
-        return Response()
-
+        # Дописать сохранение пути файла в базу
+        # Дописать сохранение в папку пользователя
+        # Вернуть нормальный респонс
+        # Размер файла
+        # поменял fs = .....
+        try:
+            user_id = request.user.id
+            user = User.objects.get(pk=request.user.id)
+            request_file = request.FILES['file']
+            fs = FileSystemStorage(location=f'{BASE_DIR_STORAGE}{user.path}', base_url=user.path)
+            file = fs.save(request_file.name, request_file)
+            file_url = fs.url(file)
+            file_size = fs.size(file)
+            new_file = File.objects.create(file_name=request_file.name,
+                                           description=request.FILES['description'],
+                                           file_path=file_url,
+                                           file_size=file_size
+                                           )
+            new_file.save()
+            return create_response(status.HTTP_200_OK,
+                                   f'Файл {request_file.name} загружен как файл {new_file.name}',
+                                   True,
+                                   {'file': request_file.name})
+        except Exception as e:
+            return create_response(status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                   str(e))
 
 
 @api_view(['DELETE'])
@@ -188,31 +202,69 @@ def delete_file(request):
             return create_response(status.HTTP_500_INTERNAL_SERVER_ERROR, str(e))
 
 
-# @api_view(['PUT'])
-# @permission_classes([IsAuthenticated])
-# def rename_file(request):
-#     if request.method == 'PUT':
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def rename_file(request):
+    if request.method == 'PUT':
+        try:
+            previous_file_name = File.objects.filter(pk=request.data['id']).values('file_name')
+            serializer = FileSerializer(data=request.data)
+            new_file_name = serializer.initial_data['file_name']
+            File.objects.filter(pk=serializer.initial_data['id']).update(file_name=new_file_name)
+            return create_response(status.HTTP_200_OK,
+                                   'Имя файла изменено!',
+                                   True,
+                                   {'previous file name': previous_file_name,
+                                    'new_file_name ': new_file_name})
+        except Exception as e:
+            return create_response(status.HTTP_500_INTERNAL_SERVER_ERROR, str(e))
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def edit_description_file(request):
+    if request.method == 'PUT':
+        try:
+            serializer = FileSerializer(data=request.data)
+            File.objects.filter(pk=serializer.initial_data['id']).update(file_name=serializer.initial_data['description'])
+            file = File.objects.get(pk=serializer.initial_data['id'])
+            return create_response(status.HTTP_200_OK,
+                                   'Обновление описания файла',
+                                   True,
+                                   {'file_name': file.file_name,
+                                    'new_description': file.description})
+        except Exception as e:
+            return create_response(status.HTTP_500_INTERNAL_SERVER_ERROR, str(e))
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def download_file(request):
+    if request.method == 'GET':
+        # заменить данные файла
+        file_path = 'text.txt'
+        filename = 'text.txt'
+
+        return create_file_response(file_path, filename)
 #
 #
-# @api_view(['PUT'])
-# @permission_classes([IsAuthenticated])
-# def edit_description_file(request):
-#     if request.method == 'PUT':
-#
-#
-# @api_view([''])
-# @permission_classes([IsAuthenticated])
-# def download_file(request):
-#     if request.method == '':
-#
-#
-# @api_view([''])
-# @permission_classes([IsAuthenticated])
-# def creating_link_to_the_file(request):
-#     if request.method == '':
-#
-#
-# @api_view([''])
-# @permission_classes([IsAuthenticated])
-# def download_file_from_link(request):
-#     if request.method == '':
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def creating_link_to_the_file(request):
+    if request.method == 'GET':
+        length = 20
+        characters = string.ascii_letters + string.digits
+        one_time_link = ''.join(random.choices(characters, k=length))
+        # созранить в базу ссылку для файла
+        # вернул респонс
+
+
+@api_view(['GET'])
+def download_file_from_link(request):
+    if request.method == 'GET':
+        one_time_link = request.GET.get['link']
+        # пошел в базу, нашел файл по ссылку
+        file_path = ''
+        filename = ''
+        response = create_file_response(file_path, filename)
+        # удалил ссылку
+        return response
