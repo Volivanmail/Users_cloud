766173e7f7ec5d656ff246db1f3addbc29fb29be
Your Name <you@example.com>
2023-12-29 13:05:07 +0300

    commit 29.12.23

--- my_cloud/settings.py
+++ my_cloud/settings.py
@@ -36,9 +36,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
