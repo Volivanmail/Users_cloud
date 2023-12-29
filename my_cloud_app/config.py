e7a8d3594d0eb908bec8ae165751f1346afbcf9f
Your Name <you@example.com>
2023-12-29 13:24:23 +0300

    commit 29.12.23/3

--- my_cloud_app/config.py
+++ my_cloud_app/config.py
@@ -5,11 +5,5 @@ config = Properties()
 with open('my_cloud_app/app_config.properties', 'rb') as config_file:
     config.load(config_file)
 
-DB_ENGINE = config.get('DB_ENGINE').data
-DB_NAME = config.get('DB_NAME').data
-DB_USER = config.get('DB_USER').data
-DB_PASSWORD = config.get('DB_PASSWORD').data
-DB_HOST = config.get('DB_HOST').data
-DB_PORT = config.get('DB_PORT').data
 BASE_DIR_STORAGE = config.get('BASE_DIR_STORAGE').data
 LENGTH_OF_THE_DOWNLOAD_LINK = config.get('LENGTH_OF_THE_DOWNLOAD_LINK').data
